f13b2a8bb940c0b187f1269fb839bd5d3716ae03
banko2 <60343173+banko2@users.noreply.github.com>
2020-02-06 06:06:25 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+            <h1 style="background-color: red ";text-align="center">Woman Gallery </h1>
+<img src="Wgalery.jpg">
+      </header> 
+
+<div class="gallery">
+  <a class="button"  href="ital3.mp4">PUSH!</a>
+
+    <img src="ital2.jpg" width="500" height="300">
+    <!-- <a class="button" type="button">PUSH!</button></a>-->
+  <div class="desc">Woman</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image1.mp4">
+    <img src="ital1.jpg"  width="500" height="400">
+  </a>
+  <div class="desc">Woman1</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="bulg.mp4">
+    <img src="bulg.jpg"  width="400" height="300">
+  </a>
+  <div class="desc">Woman2</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image3.mp4">
+    <img src="usa.jpg"  width="400" height="400">
+  </a>
+  <div class="desc">Woman3</div>
+</div>
+ 
+</body>
+</html>
